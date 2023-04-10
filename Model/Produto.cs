ab741d7fbe01c06861f57ff37c14a56711076aea
Roosevelt_Daflon <rooseveltdaflon@hotmail.com>
2023-04-10 16:47:52 -0300

    Adicionar arquivos de projeto.

---
+++ Model/Produto.cs
@@ -0,0 +1,10 @@
+﻿namespace produtos_teste.Model
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = null!;
+        public decimal Price { get; set; }
+
+    }
+}
